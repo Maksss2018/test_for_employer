2c4c459dbc856a2bd550aa034dc961c52bc33363
Maksss2018 <malyimaksym37@gmail.com>
2019-04-06 18:36:34 +0300

    Admin  role  added

--- src/components/ComponentMenu/index.js
+++ src/components/ComponentMenu/index.js
@@ -13,10 +13,10 @@ import {login, logOut} from "../../actions"
 
 const ComponentMenu = ({user,login, logOut,location,history}) => {
     let [anchor, setAnchor] =  useState(null),
-        [userStatus,setUserStatus] = useState(false);
-    useEffect(()=>{
-        setUserStatus(user);
-    },[location]);
+        [userStatus,setUserStatus] =useState(false);
+ useEffect(()=>{
+     setUserStatus(location.pathname.split("/")[1] ==="admin");
+ },[location.pathname]);
 
     const handleClick = event => {
 
@@ -43,21 +43,25 @@ const ComponentMenu = ({user,login, logOut,location,history}) => {
             >
                 <MenuItem onClick={handleClose}>
                     {
-                        location.pathname.split("/")[1] === "admin"?<Link exect to="/admin">Our menu</Link>
+                        userStatus?<Link exect to="/admin">Our menu</Link>
                             :<Link exect to="/">Our menu </Link>
                     }
 
                 </MenuItem>
-                {location.pathname ==="/admin"?<MenuItem onClick={handleClose}>
-                    <Link  to={"/admin/form"}>add new item</Link>
-                </MenuItem>:""}
+                {userStatus?<Link  to={"/admin/form"}>
+                    <MenuItem onClick={handleClose}>
+                    add new item
+                </MenuItem>
+                    </Link>:""}
+                <Link  to={userStatus?"/":"/admin"} >
                 <MenuItem  onClick={e => {
                     handleClose(e);
-                    localStorage.user = location.pathname.split("/")[1]!=="admin"?"admin":"";
+                    localStorage.user = !userStatus?"admin":"";
                 }}>
-                    {location.pathname ==="/admin"?<Link  to={"/"} > log-out</Link>
-                        :<Link  to={"/admin"} > login</Link>}
+                    {userStatus? "log-out"
+                        :"login"}
                 </MenuItem>
+                </Link>
             </Menu>
         </>
     );
